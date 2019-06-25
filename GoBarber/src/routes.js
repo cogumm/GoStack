191a950a8159a626df0303cfcdd89e36cb30cc3a
Gabriel F. Vilar <cogumm@gmail.com>
2019-06-25 14:17:23 -0300

    Iniciando back-end do GoBarber: Criando loader de models

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Estou vivo!!' }));
+routes.get('/', async (req, res) => {
+	const user = await User.create({
+		name: 'Gabriel CoGUMm',
+		email: 'gabriel@cogumm.net',
+		pass_hash: '123456',
+	});
+	res.json(user);
+});
 
 export default routes;
