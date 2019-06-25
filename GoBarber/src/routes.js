3d57882ec4008b19d06ac6d8ae4a37dac9104d73
Gabriel F. Vilar <cogumm@gmail.com>
2019-06-25 14:47:42 -0300

    Iniciando back-end do GoBarber: Cadastro de usuários

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-	const user = await User.create({
-		name: 'Gabriel CoGUMm',
-		email: 'gabriel@cogumm.net',
-		pass_hash: '123456',
-	});
-	res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
