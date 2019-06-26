a2d9c6e3b1113ac9ed2bdedef37a661b0c6e6d0b
Gabriel F. Vilar <cogumm@gmail.com>
2019-06-26 00:56:29 -0300

    Iniciando back-end do GoBarber: Validando dados de entrada

--- GoBarber/src/app/controllers/UserController.js
+++ GoBarber/src/app/controllers/UserController.js
@@ -1,7 +1,23 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
 	async store(req, res) {
+		// Validações com Yup
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string()
+				.email()
+				.required(),
+			pass: Yup.string()
+				.required()
+				.min(6),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Falha na validação.' });
+		}
+
 		const userExists = await User.findOne({
 			where: { email: req.body.email },
 		});
@@ -20,6 +36,24 @@ class UserController {
 	}
 
 	async update(req, res) {
+		const schema = Yup.object().shape({
+			name: Yup.string(),
+			email: Yup.string().email(),
+			oldPass: Yup.string().min(6),
+			pass: Yup.string()
+				.min(6)
+				.when('oldPass', (oldPass, field) =>
+					oldPass ? field.required() : field
+				),
+			confirmPass: Yup.string().when('pass', (pass, field) =>
+				pass ? field.required().oneOf([Yup.ref('pass')]) : field
+			),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Falha na validação.' });
+		}
+
 		const { email, oldPass } = req.body;
 		const user = await User.findByPk(req.userId);
 		/* ! == diferente */
