e08bd757ddcc7376b906c5aaf6b72cd7a036bff0
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-03 14:24:33 -0300

    Introdução ao React: Criando componente raiz

--- Conceitos/Introdução ao React/src/index.js
+++ Conceitos/Introdução ao React/src/index.js
@@ -1,3 +1,6 @@
-const soma = (a, b) => a + b;
+import React from "react";
+import { render } from "react-dom";
 
-alert(soma(1, 4));
+import App from "./App";
+
+render(<App />, document.getElementById("app"));
