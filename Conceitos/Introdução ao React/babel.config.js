61781f73eee0e1acc70bc69eba562bb1fc1350b3
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-03 14:18:21 -0300

    Introdução ao React: Configurando a estrutura

---
+++ Conceitos/Introdução ao React/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+        /* ENV responsável por alterar as funcionalidades do JS para que o navegador entenda
+         * Exemplo: import e exports, arrow function, algumas classes
+         */
+        "@babel/preset-env",
+
+        /* React é responsável em transformar as coisas que o navegador não entenda.
+         * Exemplo: jsx e outras funcionalidades do react
+         */
+        "@babel/preset-react"
+    ]
+};
