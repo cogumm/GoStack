18a16ac2aa1aef025cdf907dc8c89514a63ef3bb
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-04 00:59:38 -0300

    Introdução ao React: Default Props e PropTypes

--- Conceitos/Introdução ao React/src/components/TechItem.js
+++ Conceitos/Introdução ao React/src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TechItem({ tech, onDelete }) {
     return (
@@ -10,5 +11,17 @@ function TechItem({ tech, onDelete }) {
         </li>
     );
 }
+
+/*
+ * Forma global de valor dafault
+ */
+TechItem.defaultProps = {
+    tech: "Oculto"
+};
+
+TechItem.PropTypes = {
+    tech: PropTypes.string,
+    onDelete: PropTypes.func.isRequired
+};
 
 export default TechItem;
