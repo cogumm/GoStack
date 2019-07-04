810afecd2bdbe050b15d709536a012a301387689
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-04 00:45:12 -0300

    Introdução ao React: Propriedades do React

---
+++ Conceitos/Introdução ao React/src/components/TechItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TechItem({ tech, onDelete }) {
+    return (
+        <li>
+            {tech}
+            <button onClick={onDelete} type="button">
+                Remover
+            </button>
+        </li>
+    );
+}
+
+export default TechItem;
--- Conceitos/Introdução ao React/src/components/TechList.js
+++ Conceitos/Introdução ao React/src/components/TechList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TechItem from "./TechItem";
 
 class TechList extends Component {
     state = {
@@ -34,15 +35,11 @@ class TechList extends Component {
             <form onSubmit={this.handleSubmit}>
                 <ul>
                     {this.state.techs.map(tech => (
-                        <li key={tech}>
-                            {tech}
-                            <button
-                                onClick={() => this.handleDelete(tech)}
-                                type="button"
-                            >
-                                Remover
-                            </button>
-                        </li>
+                        <TechItem
+                            key={tech}
+                            tech={tech}
+                            onDelete={() => this.handleDelete(tech)}
+                        />
                     ))}
                 </ul>
                 <input
