aec384dc5579a1de4c4b231f67145df904a90db9
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-04 00:29:17 -0300

    Introdução ao React: Estado e Imutabilidade

--- Conceitos/Introdução ao React/src/components/TechList.js
+++ Conceitos/Introdução ao React/src/components/TechList.js
@@ -2,18 +2,41 @@ import React, { Component } from "react";
 
 class TechList extends Component {
     state = {
+        newTech: "",
         techs: ["NodeJS", "ReactJS", "React Native"]
     };
 
-    render() {
-        console.log(this.state);
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value });
+    };
 
+    handleSubmit = e => {
+        e.preventDefault();
+
+        //console.log(this.state.newTech);
+        this.setState({
+            techs: [...this.state.techs, this.state.newTech],
+            // Fazendo com que o input perca o valor ("limpando" o input)
+            newTech: ""
+        });
+    };
+
+    render() {
         return (
-            <ul>
-                <li>NodeJS</li>
-                <li>ReactJS</li>
-                <li>React Native</li>
-            </ul>
+            // Tag sem nome "fragment" <>
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                    {this.state.techs.map(tech => (
+                        <li key={tech}>{tech}</li>
+                    ))}
+                </ul>
+                <input
+                    type="text"
+                    onChange={this.handleInputChange}
+                    value={this.state.newTech}
+                />
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
