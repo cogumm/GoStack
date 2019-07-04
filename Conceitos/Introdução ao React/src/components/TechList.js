a5ecbe797048d3a4ce7897566e1b1008d72d43e4
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-04 01:12:08 -0300

    Introdução ao React: Ciclo de vida do componente

--- Conceitos/Introdução ao React/src/components/TechList.js
+++ Conceitos/Introdução ao React/src/components/TechList.js
@@ -4,8 +4,31 @@ import TechItem from "./TechItem";
 class TechList extends Component {
     state = {
         newTech: "",
-        techs: ["NodeJS", "ReactJS", "React Native"]
+        techs: []
     };
+
+    // Ciclo de vida
+    // Executado assim que o componente aparece na tela
+    componentDidMount() {
+        const techs = localStorage.getItem("techs");
+
+        if (techs) {
+            this.setState({ techs: JSON.parse(techs) });
+        }
+    }
+
+    // Executado sempre que houver alterações nas props ou estado
+    componentDidUpdate(_, prevState) {
+        // prevProps
+        //this.props, this.state
+        if (prevState.techs !== this.state.techs) {
+            localStorage.setItem("techs", JSON.stringify(this.state.techs));
+        }
+    }
+
+    // Executado quando o componente deixa de existir
+    // Utilizado quando tiver que ficar ouvindo 'listen' algum evento
+    componentWillMount() {}
 
     handleInputChange = e => {
         this.setState({ newTech: e.target.value });
