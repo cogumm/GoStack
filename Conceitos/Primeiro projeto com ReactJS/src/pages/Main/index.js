a30951753b60bb1d68414dabe1b11ede5d6995c5
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-09 17:43:50 -0300

    Primeiro ReactJS: Exibindo os repositórios

--- Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
+++ Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
@@ -3,8 +3,8 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
-
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container/';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
