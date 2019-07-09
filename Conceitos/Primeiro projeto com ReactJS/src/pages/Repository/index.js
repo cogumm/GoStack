1fb99bab4f537ab01c8e03c7e997b3bca02b846f
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-09 17:13:04 -0300

    Primeiro ReactJS: Carregando dados da API

--- Conceitos/Primeiro projeto com ReactJS/src/pages/Repository/index.js
+++ Conceitos/Primeiro projeto com ReactJS/src/pages/Repository/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+        const repoName = decodeURIComponent(match.params.repository);
+
+        // console.log(repoName);
+        // const response = await api.get(`/repos/${repoName}`);
+        // const issues = await api.get(`/repos/${repoName}/issues`);
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5,
+                },
+            }),
+        ]);
+        // só passa para a próxima linha depois que as duas finalizarem
+        // console.log(repository);
+        // console.log(issues);
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        const { repository, issues, loading } = this.state;
+        return <h1>Repository</h1>;
+    }
 }
