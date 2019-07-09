4d7ac341d14c3ac1139544a41d74f610e13f5f7f
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-09 16:41:16 -0300

    Primeiro ReactJS: Listando repositórios

--- Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
+++ Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
@@ -3,7 +3,7 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
@@ -37,7 +37,7 @@ export default class Main extends Component {
     };
 
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, repositories, loading } = this.state;
 
         return (
             <Container>
@@ -61,6 +61,15 @@ export default class Main extends Component {
                         )}
                     </SubmitButton>
                 </Form>
+
+                <List>
+                    {repositories.map(rep => (
+                        <li key={rep.name}>
+                            <span>{rep.name}</span>
+                            <a href="#">Detalhes</a>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
--- Conceitos/Primeiro projeto com ReactJS/src/pages/Main/styles.js
+++ Conceitos/Primeiro projeto com ReactJS/src/pages/Main/styles.js
@@ -70,3 +70,26 @@ export const SubmitButton = styled.button.attrs(props => ({
             }
         `}
 `;
+
+export const List = styled.ul`
+    list-style: none;
+    margin-top: 30px;
+
+    li {
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        /* existe um li antes? */
+        & + li {
+            border-top: 1px solid #eee;
+        }
+
+        a {
+            color: #008f95;
+            text-decoration: none;
+        }
+    }
+`;
