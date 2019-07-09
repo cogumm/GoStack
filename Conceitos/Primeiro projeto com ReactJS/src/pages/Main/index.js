c3d0eb101faf257cbb646b16c79e66f470584a24
Gabriel F. Vilar <cogumm@gmail.com>
2019-07-09 10:51:54 -0300

    Primeiro ReactJS: Estilos globais

--- Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
+++ Conceitos/Primeiro projeto com ReactJS/src/pages/Main/index.js
@@ -3,10 +3,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-    return (
-        <Title error>
-            Main
-            <small>menor</small>
-        </Title>
-    );
+    return <Title>Estou vivo!</Title>;
 }
