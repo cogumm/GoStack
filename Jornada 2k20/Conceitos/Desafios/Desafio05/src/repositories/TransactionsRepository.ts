0d9799b2d9afb1a2d86647a05d5e335e9b700b92
Gabriel F. Vilar <gabriel@cogumm.net>
2020-06-13 11:20:45 -0300

    :rotating_light: - Desafio 05 - Teste 100% ✔

---
+++ Jornada 2k20/Conceitos/Desafios/Desafio05/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,70 @@
+import Transaction from "../models/Transaction";
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    // TODO
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case "income":
+            accumulator.income += transaction.value;
+            break;
+          case "outcome":
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    // TODO
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
