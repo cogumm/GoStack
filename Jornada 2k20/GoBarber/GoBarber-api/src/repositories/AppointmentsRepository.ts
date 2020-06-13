bdd7365ebbd528e694abd3551fa40da91a936c6a
Gabriel F. Vilar <gabriel@cogumm.net>
2020-06-13 18:33:28 -0300

    :recycle: - Criando model de agendamento

--- Jornada 2k20/GoBarber/GoBarber-api/src/repositories/AppointmentsRepository.ts
+++ Jornada 2k20/GoBarber/GoBarber-api/src/repositories/AppointmentsRepository.ts
@@ -1,5 +1,5 @@
 import { isEqual } from "date-fns";
-import Appointment from "../models/Appointments";
+import Appointment from "../models/Appointment";
 
 interface CreateAppointmentDTO {
     provider: string;
