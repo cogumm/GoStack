30ead7bf3f5b68e131d251982cf1c2bb67b795ba
Gabriel F. Vilar <gabriel@cogumm.net>
2020-06-24 10:41:04 -0300

    :sparkles::art: - GoBarber Unform e usabilidade

--- Jornada 2k20/GoBarber/GoBarber-web/src/components/Input/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-web/src/components/Input/index.tsx
@@ -1,5 +1,12 @@
-import React, { InputHTMLAttributes } from "react";
+import React, {
+    InputHTMLAttributes,
+    useEffect,
+    useState,
+    useRef,
+    useCallback,
+} from "react";
 import { IconBaseProps } from "react-icons";
+import { useField } from "@unform/core";
 
 import { Container } from "./styles";
 
@@ -8,13 +15,47 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-    <>
-        <Container>
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+
+    // SEMPRE que for criar uma função dentro de um componente utilizar o useCallback
+    // Função para deixar o icone colorido.
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+
+        // Se tiver algum valor vem true
+        setIsFilled(!!inputRef.current?.value);
+    }, []);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: "value",
+        });
+    }, [fieldName, registerField]);
+
+    return (
+        <Container isFilled={isFilled} isFocused={isFocused}>
             {Icon && <Icon size={20} />}
-            <input {...rest} />
+            <input
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                defaultValue={defaultValue}
+                ref={inputRef}
+                {...rest}
+            />
         </Container>
-    </>
-);
+    );
+};
 
 export default Input;
--- Jornada 2k20/GoBarber/GoBarber-web/src/pages/SignUp/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
 
 import logo from "../../assets/logo.svg";
 
@@ -8,33 +9,39 @@ import Button from "../../components/Button";
 
 import { Container, Content, Background } from "./styles";
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logo} alt="GoBarber" />
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
 
-            <form>
-                <h1>Faça seu cadastro</h1>
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logo} alt="GoBarber" />
 
-                <Input name="name" icon={FiUser} placeholder="Seu nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    type="password"
-                    placeholder="Senha"
-                />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
 
-                <Button type="submit">Cadastrar</Button>
-            </form>
+                    <Input name="name" icon={FiUser} placeholder="Seu nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
 
-            <a href="a">
-                <FiArrowLeft />
-                Voltar para entrar
-            </a>
-        </Content>
-    </Container>
-);
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+
+                <a href="a">
+                    <FiArrowLeft />
+                    Voltar para entrar
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
