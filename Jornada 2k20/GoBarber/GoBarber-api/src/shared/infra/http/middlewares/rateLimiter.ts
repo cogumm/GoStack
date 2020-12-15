bfa9edde233eccbd3c4305ea74b422fd4bca443a
Gabriel F. Vilar <gabriel@cogumm.net>
2020-12-15 14:18:35 -0300

    :wrench::recycle: - GoBarber api - Rate limiter + redis

---
+++ Jornada 2k20/GoBarber/GoBarber-api/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import redis from "redis";
+import { Request, Response, NextFunction } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+
+import AppError from "@shared/errors/AppError";
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "ratelimit",
+    points: 5,
+    duration: 1,
+});
+
+export default async function rateLimiter(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> {
+    try {
+        await limiter.consume(req.ip);
+
+        return next();
+    } catch (err) {
+        throw new AppError("Too many requests", 429);
+    }
+}
