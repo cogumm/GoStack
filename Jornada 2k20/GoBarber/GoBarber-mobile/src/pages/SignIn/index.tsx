f9e6ae35be6b8068decc6249fb2ccc8ffaa42fc6
Gabriel F. Vilar <gabriel@cogumm.net>
2020-12-15 22:19:43 -0300

    :wrench: - GoBarber mobile - Tela de autenticação

--- Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/SignIn/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/SignIn/index.tsx
@@ -1,31 +1,77 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+    Image,
+    View,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
     return (
-        <Container>
-            <Image source={logoImg} />
-
-            <Title>Faça seu login</Title>
-
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-
-            <Button
-                onPress={() => {
-                    console.log('vai');
-                }}
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
             >
-                Entrar
-            </Button>
-        </Container>
+                <ScrollView
+                    contentContainerStyle={{ flex: 1 }}
+                    keyboardShouldPersistTaps="handled"
+                >
+                    <Container>
+                        <Image source={logoImg} />
+
+                        <View>
+                            <Title>Faça seu login</Title>
+                        </View>
+
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input
+                            name="password"
+                            icon="lock"
+                            placeholder="Senha"
+                        />
+
+                        <Button
+                            onPress={() => {
+                                console.log('vai');
+                            }}
+                        >
+                            Entrar
+                        </Button>
+
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>
+                                Esqueci minha senha
+                            </ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+
+            <CreateAccountButton onPress={() => {}}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>
+                    Criar uma conta
+                </CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
     );
 };
 
