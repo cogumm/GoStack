7186a1d252a9949ff95c98e60f31476537c37ef5
Gabriel F. Vilar <gabriel@cogumm.net>
2020-12-28 16:07:13 -0300

    :sparkles: - GoBarber web - Listagem de agendamentos

--- Jornada 2k20/GoBarber/GoBarber-web/src/pages/Dashboard/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-web/src/pages/Dashboard/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiClock, FiPower } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 import { useAuth } from "../../hooks/auth";
 
-import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Section, Appointment, Calendar } from './styles';
 
 const Dashboard: React.FC = () => {
+    // Quando clicar no dia do calendário alterar a listagem dos agendamentos.
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     const { singOut, user } = useAuth();
 
     // console.log(user);
@@ -53,6 +56,52 @@ const Dashboard: React.FC = () => {
                             </span>
                         </div>
                     </NextAppointment>
+
+                    <Section>
+                        <strong>Manhã</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                8:00
+                            </span>
+
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/113603?s=460&u=80ff02187db0c2f433149af604ca94f5333ba0c1&v=4" alt="aaa" />
+
+                                <strong>Gabriel</strong>
+                            </div>
+                        </Appointment>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                9:00
+                            </span>
+
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/113603?s=460&u=80ff02187db0c2f433149af604ca94f5333ba0c1&v=4" alt="aaa" />
+
+                                <strong>Gabriel</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+
+                    <Section>
+                        <strong>Tarde</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                8:00
+                            </span>
+
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/113603?s=460&u=80ff02187db0c2f433149af604ca94f5333ba0c1&v=4" alt="aaa" />
+
+                                <strong>Gabriel</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
                 </Schedule>
                 <Calendar />
             </Content>
