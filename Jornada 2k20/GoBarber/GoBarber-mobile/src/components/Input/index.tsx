30cf5a0c3b1aa96fafef7552b8d14a6d6e748998
Gabriel F. Vilar <gabriel@cogumm.net>
2021-01-11 15:58:25 -0300

    :sparkles::recycle: - GoBarber mobile - Foco e preenchimento

--- Jornada 2k20/GoBarber/GoBarber-mobile/src/components/Input/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/components/Input/index.tsx
@@ -1,9 +1,12 @@
 import React, {
+  useState,
   useEffect,
   useRef,
   useImperativeHandle,
   forwardRef,
+  useCallback,
 } from 'react';
+
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -30,6 +33,19 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
 
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
 
   /**
    * useImperativeHandle: Serve para passar uma função de um componente interno/filho para um componente pai.
@@ -63,14 +79,20 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         // Teclado em modo dark no iOS, se não for iOS o teclado vem padrão.
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
--- Jornada 2k20/GoBarber/GoBarber-mobile/src/components/Input/styles.ts
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/components/Input/styles.ts
@@ -1,25 +1,38 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
-    width: 100%;
-    height: 60px;
-    padding: 0 16px;
-    background: #232129;
-    border-radius: 10px;
-    margin-bottom: 8px;
+interface ContainerProps {
+  isFocused: boolean;
+}
 
-    flex-direction: row;
-    align-items: center;
+export const Container = styled.View<ContainerProps>`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #232129;
+  border-radius: 10px;
+  margin-bottom: 8px;
+
+  border-width: 2px;
+  border-color: #232129;
+
+  flex-direction: row;
+  align-items: center;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`
-    flex: 1;
-    color: #fff;
-    font-size: 16px;
-    font-family: 'RobotoSlab-Regular';
+  flex: 1;
+  color: #fff;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
 `;
 
 export const Icon = styled(FeatherIcon)`
-    margin-right: 16px;
+  margin-right: 16px;
 `;
