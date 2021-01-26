56c3c0adaac8bfac5921983767883a576b2c4c58
Gabriel F. Vilar <gabriel@cogumm.net>
2021-01-26 11:25:13 -0300

    :sparkles: - GoBarber mobile - Header do Dashboard

--- Jornada 2k20/GoBarber/GoBarber-mobile/src/hooks/auth.tsx
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/hooks/auth.tsx
@@ -9,9 +9,15 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -20,7 +26,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
