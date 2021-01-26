0c56a0a551a69e5095b8601af083d5d1745ba851
Gabriel F. Vilar <gabriel@cogumm.net>
2021-01-26 10:57:42 -0300

    :sparkles: - GoBarber mobile - Criando páginas e rotas

--- Jornada 2k20/GoBarber/GoBarber-mobile/src/routes/app.routes.tsx
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -12,7 +15,12 @@ const AppRoutes: React.FC = () => (
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
+    {/* Fluxo das telas */}
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
