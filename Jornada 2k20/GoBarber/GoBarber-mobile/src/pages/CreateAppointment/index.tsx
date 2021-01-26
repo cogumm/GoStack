ab8134f1291e39b9808d4e8c152ab45c75eccd3b
Gabriel F. Vilar <gabriel@cogumm.net>
2021-01-26 15:52:32 -0300

    :sparkles: - GoBarber mobile - Alternando entre providers

--- Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/CreateAppointment/index.tsx
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,10 +11,21 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles';
 
 interface RouteParams {
   providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
 }
 
 const CreateAppointment: React.FC = () => {
@@ -21,14 +33,29 @@ const CreateAppointment: React.FC = () => {
 
   const route = useRoute();
   // console.log(route.params);
-  const { providerId } = route.params as RouteParams;
+  const routeParams = route.params as RouteParams;
   // console.log(routeParams.providerId);
 
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
+
   const { goBack } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(res => {
+      setProviders(res.data);
+    });
+  }, []);
 
   const navigateBack = useCallback(() => {
     goBack();
   }, [goBack]);
+
+  const handleSelectedProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
 
   return (
     <Container>
@@ -40,6 +67,28 @@ const CreateAppointment: React.FC = () => {
         <HeaderTitle>Cabeleireiros/Barbeiros</HeaderTitle>
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          // Scrool horizontal
+          horizontal
+          // Para não mostrar a barra do scroll
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectedProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/CreateAppointment/styles.ts
+++ Jornada 2k20/GoBarber/GoBarber-mobile/src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { Provider } from './index';
+
+interface ProviderContainerProps {
+  selected: boolean;
+}
+
+interface ProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -19,9 +31,9 @@ export const BackButton = styled.TouchableOpacity``;
 
 export const HeaderTitle = styled.Text`
   font-family: 'RobotoSlab-Medium';
-  color: #f5ede8;
   font-size: 18px;
   margin-left: 16px;
+  color: #f5ede8;
 `;
 
 export const UserAvatar = styled.Image`
@@ -29,4 +41,34 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  margin-left: 8px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `;
