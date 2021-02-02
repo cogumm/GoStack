ab6c1957ea0095ff384cb6523774b975c150d0c0
Gabriel F. Vilar <gabriel@cogumm.net>
2021-02-02 17:59:39 -0300

    :rotating_light: - GoBarber web - Criando mock da API

--- Jornada 2k20/GoBarber/GoBarber-web/src/__tests__/hooks/auth.spec.tsx
+++ Jornada 2k20/GoBarber/GoBarber-web/src/__tests__/hooks/auth.spec.tsx
@@ -1,10 +1,27 @@
 import { renderHook } from "@testing-library/react-hooks";
+import MockAdapter from "axios-mock-adapter";
 
 import { useAuth, AuthProvider } from "../../hooks/auth";
+import api from "../../services/api";
+
+const apiMock = new MockAdapter(api);
 
 describe("Auth hook", () => {
     it("should be able to sign in", async () => {
-        const { result } = renderHook(() => useAuth(), {
+        const apiRes = {
+            user: {
+                id: "user-123",
+                name: "John Doe",
+                email: "john@doe.com",
+            },
+            token: "token-123",
+        };
+
+        apiMock.onPost("sessions").reply(200, apiRes);
+
+        const setItemSpy = jest.spyOn(Storage.prototype, "setItem");
+
+        const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
             wrapper: AuthProvider,
         });
 
@@ -12,6 +29,17 @@ describe("Auth hook", () => {
             email: "john@doe.com",
             password: "123456",
         });
+
+        await waitForNextUpdate();
+
+        expect(setItemSpy).toHaveBeenCalledWith(
+            "@GoBarber:token",
+            apiRes.token,
+        );
+        expect(setItemSpy).toHaveBeenCalledWith(
+            "@GoBarber:user",
+            JSON.stringify(apiRes.user),
+        );
 
         expect(result.current.user.email).toEqual("john@doe.com");
     });
